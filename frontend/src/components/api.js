71adb419858179a4c8c358f3ca02918c80d88efc
Ant <tonyagoryanova@gmail.com>
2023-05-11 12:45:10 +0900

    adding server's link

--- frontend/src/components/api.js
+++ frontend/src/components/api.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const SearchWeather = async (city) => {
-  const weather = await axios.get(`http://localhost:8000/weather?city=${city}`);
+  // const weather = await axios.get(`http://localhost:8000/weather?city=${city}`);
+  const weather = await axios.get(
+    `https://weather-app-suqh.onrender.com/weather?city=${city}`
+  );
 
   return weather.data;
 };
--- frontend/src/components/backgroundApi.js
+++ frontend/src/components/backgroundApi.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
 const searchImages = async (city) => {
-  const options = await axios.get(`http://localhost:8000/image?city=${city}`);
+  // const options = await axios.get(`http://localhost:8000/image?city=${city}`);
+  const options = await axios.get(
+    `https://weather-app-suqh.onrender.com/image?city=${city}`
+  );
+
   return options.data.results;
 };
 
